7ed7498c9cc2b71bf3065dfcb53054bb91372e5b
shige <44439885+geshi-geshi@users.noreply.github.com>
2019-11-25 22:23:31 +0900

    ログイン機能追加 (#14)
    * ユーザーページ追加、Bootstrap追加
    
    * ヘッダー追加
    
    * ユーザー登録機能追加
    
    * ログイン機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
   def top
   end
+
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user # 保存成功後、ログインします。
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user # redirect_to user_url(@user)と同様
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'users#top'
-  get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
 end
