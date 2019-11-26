ba197205d5d651418de83f6fa5bba68cefe0915b
geshi-geshi <shige22000200021@gmail.com>
2019-11-27 00:10:25 +0900

    ローカルでMySQLを使うようにした

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_11_25_080041) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "studies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "day"
+    t.integer "study_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.boolean "admin", default: false
