1047eb83f4d505c98ed1e89248376993d185ae8f
geshi-geshi <shige22000200021@gmail.com>
2019-12-05 15:44:34 +0900

    ワイン登録機能を追加

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,10 +1,29 @@
 class WinesController < ApplicationController
-  def new
-    @wine = Wine.new
-  end
 
   def index
     @wines = Wine.paginate(page: params[:page])
   end
+
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    if @wine.save
+      flash[:success] = 'ワインを新規登録しました。'
+      redirect_to wines_url 
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:name, :color, :body, :country, :region, :area, :grape_variety, :appearance, :aroma, :taste, :production_year, :alcohol_content, :price, :detail)
+  end
+
+ 
 
 end
