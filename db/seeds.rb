b891f945337092f0ef896a1d0aa9b8cd1e727d98
geshi-geshi <shige22000200021@gmail.com>
2019-12-09 15:18:03 +0900

    heroku用ににコメントアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
-User.create!(
-    name: "管理者ユーザー",
-    email: "testadmin@sample.com",
-    admin: true,
-    password:              "testpassword",
-    password_confirmation: "testpassword")
+# User.create!(
+#     name: "管理者ユーザー",
+#     email: "testadmin@sample.com",
+#     admin: true,
+#     password:              "testpassword",
+#     password_confirmation: "testpassword")
 
-User.create!(
-    name: "テストユーザー",
-    email: "test@sample.com",
-    password:              "testpassword",
-    password_confirmation: "testpassword")
+# User.create!(
+#     name: "テストユーザー",
+#     email: "test@sample.com",
+#     password:              "testpassword",
+#     password_confirmation: "testpassword")
 
-60.times do |n|
-    name  = Faker::Name.name
-    email = "sample-#{n+1}@email.com"
-    password = "password"
-    User.create!(name: name,
-                    email: email,
-                    password: password,
-                    password_confirmation: password)
-end
+# 60.times do |n|
+#     name  = Faker::Name.name
+#     email = "sample-#{n+1}@email.com"
+#     password = "password"
+#     User.create!(name: name,
+#                     email: email,
+#                     password: password,
+#                     password_confirmation: password)
+# end
 
 Wine.create!(
     name: "ロマネ・コンティ",
