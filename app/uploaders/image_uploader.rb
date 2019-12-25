3285b6891ef43fba6b9e36d789871916d772ff96
geshi-geshi <shige22000200021@gmail.com>
2019-12-25 13:54:34 +0900

    s3に画像アップロードできるようにした

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+
+  # developmentとtest環境はlocalファイル、それ以外（productionやstaging）はS3を使う設定をする
+  # if Rails.env.development?
+  #   storage :file
+  # elsif Rails.env.test?
+  #   storage :file
+  # else
+    storage :fog
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -35,15 +42,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    original_filename if original_filename
+  end
 
   # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
@@ -55,17 +62,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   process :convert => 'jpg'
  
   # サムネイルを生成する設定
-  version :thumb do
-    process :resize_to_limit => [250, 250]
-  end
+  # version :thumb do
+  #   process :resize_to_limit => [250, 250]
+  # end
    
-  version :thumb150 do
-    process :resize_to_limit => [150, 150]
-  end
+  # version :thumb150 do
+  #   process :resize_to_limit => [150, 150]
+  # end
  
-  version :thumb30 do
-    process :resize_to_limit => [30, 30]
-  end
+  # version :thumb30 do
+  #   process :resize_to_limit => [30, 30]
+  # end
  
   # jpg,jpeg,gif,pngしか受け付けない
   def extension_white_list
