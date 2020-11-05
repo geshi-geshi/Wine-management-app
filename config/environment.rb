4c62767c5f972ce922aff027333c9f2e2fa0766a
geshi-geshi <shige22000200021@gmail.com>
2020-11-05 04:07:33 +0000

    aws用に編集

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ require_relative 'application'
 Rails.application.initialize!
 
 # ページネーションラベルの設定
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = '>'
+#WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<'
+#WillPaginate::ViewHelpers.pagination_options[:next_label] = '>'
